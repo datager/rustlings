98d2c4919f270ce8866d562a117494ea0fdd32c8
y <yuchuansun@deepglint.comm>
2024-07-28 00:19:55 +0800

    update

---
+++ target/debug/deps/move_semantics4-9d324f0a128f616b.d
@@ -0,0 +1,5 @@
+/Users/y/Desktop/rustlings/target/debug/deps/move_semantics4-9d324f0a128f616b: exercises/06_move_semantics/move_semantics4.rs
+
+/Users/y/Desktop/rustlings/target/debug/deps/move_semantics4-9d324f0a128f616b.d: exercises/06_move_semantics/move_semantics4.rs
+
+exercises/06_move_semantics/move_semantics4.rs:
---
+++ target/debug/deps/move_semantics5-364c936237d864e9.d
@@ -0,0 +1,5 @@
+/Users/y/Desktop/rustlings/target/debug/deps/move_semantics5-364c936237d864e9: exercises/06_move_semantics/move_semantics5.rs
+
+/Users/y/Desktop/rustlings/target/debug/deps/move_semantics5-364c936237d864e9.d: exercises/06_move_semantics/move_semantics5.rs
+
+exercises/06_move_semantics/move_semantics5.rs:
